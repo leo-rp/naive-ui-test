5fa2fa17fd930a940b1712a9e75431e2faca8664
leo-rp <leorock1988@hotmail.com>
2022-07-30 15:41:41 -0500

    config naive-ui globally

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
+/*
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
 
 createApp(App).mount('#app')
+*/
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import naive from "naive-ui";
+//import './assets/main.css'
+
+const app = createApp(App);
+app.use(naive);
+app.mount("#app");
