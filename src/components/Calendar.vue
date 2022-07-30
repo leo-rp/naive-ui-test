011536cbce247cc95a359d1be92a2ed2392cb873
leo-rp <leorock1988@hotmail.com>
2022-07-30 15:42:25 -0500

    the naive-ui components!

---
+++ src/components/Calendar.vue
@@ -0,0 +1,32 @@
+<script>
+  import { defineComponent } from 'vue'
+  
+  // theme
+  import { createTheme, inputDark, datePickerDark } from 'naive-ui'
+  // locale & dateLocale
+  //import { zhCN, dateZhCN } from 'naive-ui'
+
+  export default defineComponent({
+    
+    setup() {
+      return {
+        darkTheme: createTheme([inputDark, datePickerDark]),
+        //zhCN,
+        //dateZhCN
+      }
+    }
+  })
+</script>
+
+<template>
+  <n-config-provider >
+    <n-space vertical>
+      <n-input />
+      <n-date-picker />
+    </n-space>
+  </n-config-provider>
+</template>
+
+<style>
+  
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+
+
+  <n-gradient-text type="info">
+    the Vue3 
+  </n-gradient-text>
+  <n-gradient-text type="danger">
+     + 
+  </n-gradient-text>
+  
+  <n-gradient-text :size="24" type="warning">
+     Naive-ui 
+  </n-gradient-text>
+  
+  <n-gradient-text :size="24" type="success">
+     test WSS!
+  </n-gradient-text>
+
+  <n-space>
+    <span style="font-variant-numeric: tabular-nums">
+      <n-countdown :duration="5000" :active="active" />
+    </span>
+    <n-switch v-model:value="active" />
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+
+export default defineComponent({
+	
+  setup () {
+    return {
+      active: ref(false)
+    }
+  }
+})
+</script>
+
+<style scoped>
+.n-gradient-text {
+  font-size: 36px;
+}
+</style>
