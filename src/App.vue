f2a9942277c68d68848c92c756c9b347fc863ff9
leo-rp <leorock1988@hotmail.com>
2022-08-01 16:44:35 -0500

    cleaning the main struct

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<template >
-  <n-config-provider>
-    <n-loading-bar-provider>
-      <n-message-provider>
-        <n-notification-provider>
-          <n-dialog-provider>
-            <div class="wrapper">
-				<HelloWorld msg="the Vue3 + Naive-ui  test WSS!" />				
-			</div>	
-			<div class="wrapper">
-				<Login />
-			</div>	
-			<div class="wrapper">
-				<Calendar />
-			</div>	
-			<div class="wrapper">
-				<Table />
-			</div>
-			<div class="wrapper">
-				<Calendar_n />
-			</div>			
-          </n-dialog-provider>
-        </n-notification-provider>
-      </n-message-provider>
-    </n-loading-bar-provider>
-  </n-config-provider>
+<template >	
+	<n-message-provider>	
+		<Login />
+		<HelloWorld msg="the Vue3 + Naive-ui  test WSS!" />				
+		<Calendar />
+		<Table />
+		<Calendar_n />	
+	</n-message-provider>
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -44,9 +27,6 @@ export default {
    Calendar,
    Calendar_n,
    Table
-  
-   
-  
   },
 
   data: () => ({
